dcb6ec2e3b9f56dab7e853fc130f9883eeb7146f
Pascale Beier <mail@pascalebeier.de>
2017-05-30 10:59:54 +0200

    Update DevDependencies and Build System

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 2.0.1
+
+* Update devDependencies and Build System
+
 ## 2.0.0
 
 * Completely Rewritten
--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,5 @@ module.exports = {
         loader: 'babel-loader'
       }
     ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+  }
 };
